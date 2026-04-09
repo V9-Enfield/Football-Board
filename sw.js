1286f583e8239fbb5aaf39f49c9b1a7ba828b281
V9-Enfield <vcingh@gmail.com>
2026-04-09 10:00:44 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,16 @@
-const CACHE = 'tactics-v1';
+const CACHE = 'tactics-v2';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS))
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
   );
 });
 
